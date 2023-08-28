f5916d3322dd868b0c636d3528f102315c59af1f
Mike Tagg <mdtagg91@gmail.com>
2023-08-28 11:06:38 -0700

    initial commit

---
+++ formulas.js
@@ -0,0 +1,16 @@
+
+const add = (a,b) => {
+    return a + b
+}
+
+const multiply = (a,b) => {
+    return a * b
+}
+
+const subtract = (a,b) => {
+    return a - b
+}
+
+const divide = (a,b) => {
+    return a / b
+}
